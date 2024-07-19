a2f12d7626eeb425be121efe0cc33ca4947a5b84
Ahmed Yasser Singap <130315264+singap88a@users.noreply.github.com>
2024-07-19 09:34:29 -0700

    Add files via upload

---
+++ len.js
@@ -0,0 +1,71 @@
+ 
+let loginForm = document.querySelector(".login-form")
+ 
+ 
+ 
+function showform(){
+    loginForm.classList.add("active")
+}
+function hideform(){
+    loginForm.classList.remove("active")
+
+}
+
+// ////// 
+
+// let loginFormm = document.querySelector(".login-form1")
+
+// function xxx(){
+
+//     loginFormm.classList.add("active")
+// }
+///////// 
+
+
+let singap = document.querySelector(".login-form2")
+ 
+ 
+function mmm(){
+    singap.classList.add("active")
+    
+}
+// //////// 
+ 
+let loginFormx = document.querySelector(".login-form2")
+
+function hideformm(){
+    loginFormx.classList.remove("active")
+
+}
+////////// 
+let loginFormb = document.querySelector(".login-form3")
+ 
+ 
+function singa(){
+    loginFormb.classList.add("active")
+    
+}
+// /////////////////////////////////////////
+let loginFormv = document.querySelector(".login-form3")
+
+function hideformmvv(){
+    loginFormv.classList.remove("active")
+
+}
+////////////////////////////////////////////
+let loginFor = document.querySelector(".mas")
+
+function masedg(){
+    loginFor.classList.add("active")
+
+}
+// /////////////////////////////////////////
+let searchBtn = document.querySelector("#search-btn")
+function showbar(){
+    searchBtn.classList.toggle("fa-times")
+    loginFor.classList.toggle("active")
+}
+
+
+
+ 
